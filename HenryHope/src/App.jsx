9d60ea6eb0f2a81ee3eec0c6f6abc6b9d9c5bbf3
Ghost <105050482+CJNdukwe@users.noreply.github.com>
2023-11-23 14:17:29 +0100

    environment set up

---
+++ HenryHope/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+
+
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        
+       </Routes>
+     </> 
+  )
+}
+
+export default App;
