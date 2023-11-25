fa46c460b75ee07db67bc226dcd09a178a53403b
Ghost <105050482+CJNdukwe@users.noreply.github.com>
2023-11-26 00:40:29 +0100

    homepage about us and service section desktop view

--- HenryHope/src/Components/Homepage/About Us/about-us-home.jsx
+++ HenryHope/src/Components/Homepage/About Us/about-us-home.jsx
@@ -1,24 +1,66 @@
-
+import "../About Us/Style/about-us-home.css"
+import { FaIndustry, FaPeopleGroup } from "react-icons/fa6";
+import { GiPowerGenerator, GiAutoRepair, GiCommercialAirplane, GiElectric } from "react-icons/gi";
+import { TbTruckDelivery } from "react-icons/tb";
+import { TfiRulerPencil } from "react-icons/tfi";
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 function AboutUsHome() {
   return (
     <div className="about-us-home">
-      <div>
+      <div className="flex justify-between items-center">
+      <div className="flex flex-col gap-10 pl-24">
         <div>
-        <h4 className="font-primary font-extrabold text-xl text-[#000]" >
-          ABOUT US</h4>
+        <h3 className="font-primary font-extrabold text-2xl text-[#000]" >
+          ABOUT US</h3>
         </div>
         <div>
-          <h4 className="font-primary font-extrabold text-lg text-[#ff0000]">Henry Hope Engineering 
-            provides comprehensive engineering services for rural, urban and 
-            industrial electrification (commercial and residential)</h4>
+          <p className="font-secondary font-normal text-base text-[#333333]">Henry Hope Engineering 
+            provides comprehensive engineering services for <br /> rural, urban and 
+            industrial electrification (commercial and residential).</p>
         </div>
-        <div>
-          <a href="/about-us">Learn more about us</a>
+        <div className="link-to-about-us">
+          <a href="/about-us" className="font-secondary">Learn more about us</a>
         </div>
       </div>
-      <div>
-        
+      <div className="flex flex-col gap-10 justify-start items-start font-secondary p-10 bg-[#aab2d1] w-[50%]">
+        <div className="home-service-tag flex gap-5 justify-center items-center">
+          <FaIndustry className="text-2xl text-[#666]" />
+          <h4>Industrial Electrical Installation Services and Maintenance</h4>
+        </div>
+        <div className="home-service-tag flex gap-5 justify-center items-center">
+          <GiPowerGenerator className="text-2xl text-[green]" />
+          <h4>Industrial plants and generators </h4>
+        </div>
+        <div className="home-service-tag flex gap-5 justify-center items-center">
+          <FaPeopleGroup className="text-2xl text-[brown]" />
+          <h4>Engineering consultants</h4>
+        </div>
+        <div className="home-service-tag flex gap-5 justify-center items-center">
+          <TbTruckDelivery className="text-2xl text-[#000]" />
+          <h4>Supply and Installation of engineering materials</h4>
+        </div>
+        <div className="home-service-tag flex gap-5 justify-center items-center">
+          <TfiRulerPencil className="text-2xl text-[brown]" />
+          <h4>Engineering designs and drawings</h4>
+        </div>
+        <div className="home-service-tag flex gap-5 justify-center items-center">
+          <GiCommercialAirplane className="text-2xl text-[blue]" />
+          <h4>Aviation engineers</h4>
+        </div>
+        <div className="home-service-tag flex gap-5 justify-center items-center">
+          <GiElectric className="text-2xl text-[#ff0000]" />
+          <h4>Rural and urban residential and commercial electrification</h4>
+        </div>
+        <div className="home-service-tag flex gap-5 justify-center items-center">
+          <GiAutoRepair className="text-2xl text-[orange]" />
+          <h4>Electrical maintenance</h4>
+        </div>
+        <div className="link-to-services flex">
+          <a href="/services">See more details</a>
+          <FaLongArrowAltRight className="pt-2 text-[#ff0000]" />
+        </div>
+      </div>
       </div>
     </div>
   )
